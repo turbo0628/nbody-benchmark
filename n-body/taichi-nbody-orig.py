1825348816068530deb8470a75a476c7538fcc04
Haidong Lan <haidonglan@taichi.graphics>
2022-03-09 20:56:04 +0800

    Result checked. Performance has to be further optimized.

--- n-body/taichi-nbody-orig.py
+++ n-body/taichi-nbody-orig.py
@@ -1,4 +1,5 @@
 import taichi as ti
+import time
 
 ti.init(arch=ti.cuda)
 
@@ -20,7 +21,7 @@ def bodyForce():
             dy = bodies[j, 0, 1] - bodies[i, 0, 1]
             dz = bodies[j, 0, 2] - bodies[i, 0, 2]
             distSqr = dx * dx + dy * dy + dz * dz + softening
-            invDist = 1.0 / distSqr
+            invDist = 1.0 / ti.sqrt(distSqr)
             invDist3 = invDist * invDist * invDist
             Fx += dx * invDist3
             Fy += dy * invDist3
@@ -29,6 +30,7 @@ def bodyForce():
         bodies[i, 1, 1] += dt * Fy;
         bodies[i, 1, 2] += dt * Fz;
 
+    for i in range(nBodies):
         bodies[i, 0, 0] += bodies[i, 1, 0] * dt;
         bodies[i, 0, 1] += bodies[i, 1, 1] * dt;
         bodies[i, 0, 2] += bodies[i, 1, 2] * dt;
@@ -41,21 +43,18 @@ def randomizeBodies():
 @ti.kernel
 def substep():
     body_force()
-    pass
 
 def run_nbody():
-    import time
     nIters = 1024
     randomizeBodies()
-
-    bodyForce()
+    bodyForce() # warm-up
     st = time.time()
     for i in range(nIters):
         bodyForce()
+    ti.sync()
     et = time.time()
+
     avg_time =  (et - st) * 1000.0 / nIters
     print("Finishing...time {}ms".format(avg_time))
     print("Nbody speed {} Billion bodies per second.".format(1e-6 * nBodies * nBodies / avg_time))
-    print(bodies)
-
 run_nbody()
